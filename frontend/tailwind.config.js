ea2e1f8a50478019ebd184e8a106a2e3a138e786
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2021-07-17 16:45:13 +0900

    Timetable template

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        timetable: '6% repeat(7, 1px 1fr)',
+      },
+      gridTemplateRows: {
+        '31': 'repeat(31, 1fr)',
+      },
+    },
   },
   variants: {
     extend: {},
